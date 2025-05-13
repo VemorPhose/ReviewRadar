ad25cc287fa37d833d75b56d15c13480c585dfea
Vemor Phose <anonygamer666@gmail.com>
2025-05-14 02:11:08 +0530

    Fix icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
-import viteLogo from "/icon.svg";
+import viteLogo from "/icon.png";
 import "./App.css";
 
 function App() {
